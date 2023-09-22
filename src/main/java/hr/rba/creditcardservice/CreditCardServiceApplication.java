6bb46f24658ffdafa11fc1d26377ae97cc40b5de
Ante Radic <ante.radic@rba.hr>
2023-09-22 14:54:51 +0200

    Uncomment tests

--- src/main/java/hr/rba/creditcardservice/CreditCardServiceApplication.java
+++ src/main/java/hr/rba/creditcardservice/CreditCardServiceApplication.java
@@ -1,12 +1,7 @@
 package hr.rba.creditcardservice;
 
-import hr.rba.creditcardservice.security.*;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.*;
-import org.springframework.scheduling.concurrent.*;
-
-import java.util.concurrent.*;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 public class CreditCardServiceApplication {
